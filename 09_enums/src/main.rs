cb5674b9a5002c91d82d530e985f7330ca29aaad
Stefan Crummenerl <crummenerls@gmail.com>
2020-11-23 08:38:06 +0100

    Match

--- 09_enums/src/main.rs
+++ 09_enums/src/main.rs
@@ -20,4 +20,18 @@ fn main() {
 
     let m = Message::Write(String::from("hello"));
     m.call();
+
+    // ************************************************************************
+    // * Option<T> *
+    // Enum that can encode the concept of a value not being present or 
+    // absent.
+    // <T>: generic type parameter
+    // Option<T> enum is still just a regular enum, and Some(T) and None are
+    // still variants of type Option<T>;
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+
 }
---
+++ 10_match_control_flow_operator/src/main.rs
@@ -0,0 +1,49 @@
+#![allow(unused)]
+fn main() {
+    // ************************************************************************
+    // * Match *
+    // Compare a value against patterns and execute code based on mathing ones
+    // The compiler confirms that all possible cases are handled;
+
+    #[derive(Debug)]
+    enum UsState {
+        Alabame,
+        Alaska,
+        // ...
+    }
+
+    enum Coin {
+        Penny,
+        Nickle,
+        Dime,
+        Quarter(UsState),
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickle => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => 25,
+        }
+    }
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (), // Other
+    }
+}
