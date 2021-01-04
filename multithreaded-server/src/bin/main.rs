62ec450fe751b7d5ede63e1976b9a3e2921ac283
Stefan Crummenerl <crummenerls@gmail.com>
2021-01-04 18:05:28 +0100

    multithreaded-server - multithreaded_server

--- multithreaded-server/src/bin/main.rs
+++ multithreaded-server/src/bin/main.rs
@@ -1,6 +1,11 @@
+extern crate multithreaded_server;
+
 use multithreaded_server::ThreadPool;
+
 use std::fs;
 use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
@@ -8,7 +13,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
@@ -18,29 +23,24 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    // Read bytes into buffer;
-    let mut buffer = [0; 1024];
+    let mut buffer = [0; 512];
+
     stream.read(&mut buffer).unwrap();
 
-    // Indication of GET HTTP 1.1 in HTTP as variable get;
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    // If incoming byte stream, stored in buffer and read as chars[], starts
-    // with "GET HTTP 1.1 in HTTP" set status_line, filename to success etc.
-    // else to 404 etc.;
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
-    // Write concent of file to string;
     let contents = fs::read_to_string(filename).unwrap();
-
-    // Create response;
     let response = format!("{}{}", status_line, contents);
-
-    // Return;
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
