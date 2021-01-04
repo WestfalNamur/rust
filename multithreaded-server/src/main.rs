fdac629f30bf0fc3eadf90e6409a991b4ea75725
Stefan Crummenerl <crummenerls@gmail.com>
2021-01-04 16:47:58 +0100

    multithreaded-server - listen to TCP/HTTP - Respons

--- multithreaded-server/src/main.rs
+++ multithreaded-server/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -15,12 +16,29 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    // Create a 1024 byte buffer to store incoming byte-stream, convert to
-    // string and print it;
-
+    // Read bytes into buffer;
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    // Indication of GET HTTP 1.1 in HTTP as variable get;
+    let get = b"GET / HTTP/1.1\r\n";
+
+    // If incoming byte stream, stored in buffer and read as chars[], starts
+    // with "GET HTTP 1.1 in HTTP" set status_line, filename to success etc.
+    // else to 404 etc.;
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    // Write concent of file to string;
+    let contents = fs::read_to_string(filename).unwrap();
+
+    // Create response;
+    let response = format!("{}{}", status_line, contents);
+
+    // Return;
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
