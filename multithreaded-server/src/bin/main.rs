686648d71cfdf80a429ccd4aa1593d2d124d6558
Stefan Crummenerl <crummenerls@gmail.com>
2021-01-04 17:21:20 +0100

    multithreaded-server - listen to TCP/HTTP - Seperate server and threadpool

--- multithreaded-server/src/bin/main.rs
+++ multithreaded-server/src/bin/main.rs
@@ -1,16 +1,17 @@
+use multithreaded_server::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
---
+++ multithreaded-server/src/lib.rs
@@ -0,0 +1,15 @@
+pub struct ThreadPool;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        ThreadPool
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
+
+fn main() {}
